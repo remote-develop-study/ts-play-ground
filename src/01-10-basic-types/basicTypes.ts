c071b035aa8da72bbe28ba17a5bd9975428b832a
Kiwoong Kwon <descobrido@gmail.com>
2018-01-10 17:42:01 +0900

    Completed the assignment

--- src/01-10-basic-types/basicTypes.ts
+++ src/01-10-basic-types/basicTypes.ts
@@ -1,24 +1,43 @@
-export function makeString() {
+export function makeString(a: string, b: number) {
+  let firstIsString :Boolean = typeof a === 'string';
+  let secondIsNumber :Boolean = typeof b === 'number';
 
-  return;
+  if (firstIsString && secondIsNumber) {
+    return `${a} ${b}년 반가워`;
+  }
+
+  return '거절한다';
 }
 
-export function sum() {
-  return;
+export function sum(a: number, b: number) {
+  if (a && b) {
+    return a + b;
+  }
+  
+  return '두개의 숫자를 넣어주세요';
 }
 
-export function inverseBool() {
-  return;
+export function inverseBool(a: boolean) {
+  return a ? false : true;
 }
 
-export function sumOfAll() {
-  return;
+export function sumOfAll(numbers: Array<number>) {
+  let result = numbers.reduce((a, b) => a + b, 0);
+  return result;
 }
 
-export function testTuple() {
-  return;
+export function testTuple(arr: [string, boolean, number]) {
+  let result = ''
+  
+  arr.map(v => {
+    result += `${v} `
+  })
+  
+  result = result.trim();
+
+  return result;
 }
 
-export function testEnum() {
-  return;
+export function testEnum(color: Color) {
+  return color.Green;
 }
