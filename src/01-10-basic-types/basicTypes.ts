f3203f8843340e0994e4b4ae2b75af65758ceeae
Jang Hyeon-seok <devjang2016@gmail.com>
2018-01-10 23:17:44 +0900

    solve basictypes

--- src/01-10-basic-types/basicTypes.ts
+++ src/01-10-basic-types/basicTypes.ts
@@ -1,24 +1,29 @@
-export function makeString() {
-
-  return;
+export function makeString(greeting: string, year: number) {
+  return ((typeof greeting !== 'string' || typeof year !== 'number')
+            ? '거절한다'
+            : `${greeting} ${year}년 반가워`);
 }
 
-export function sum() {
-  return;
+export function sum(num1: number, num2: number) {
+  return ((!num1 || !num2)
+            ? '두개의 숫자를 넣어주세요'
+            : num1 + num2);
 }
 
-export function inverseBool() {
-  return;
+export function inverseBool(flag: boolean) {
+  return !flag;
 }
 
-export function sumOfAll() {
-  return;
+export function sumOfAll(arr: Array<number>) {
+  return arr.reduce((prev, curr) => prev + curr);
 }
 
-export function testTuple() {
-  return;
+export function testTuple(x: [string, boolean, number]) {
+  return x.join(' ');
 }
 
-export function testEnum() {
-  return;
+export function testEnum(color: Color) {
+  const colorName: string = color.Green;
+
+  return colorName;
 }
