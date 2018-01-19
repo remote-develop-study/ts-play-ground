f8e34a5fb0e7ecebab6527184932b793e371ddf6
taekyunKimDev <powerging2@nate.com>
2018-01-20 01:47:24 +0900

    2차 스프린트 시작 변수선언

---
+++ src/01-20-variable-declarations/variableDeslarations.ts
@@ -0,0 +1,29 @@
+//블록스코스 문제
+export function blockScope () {
+    var a = 40, b =20;
+    if() {
+        var c = a + b;
+    }
+    return c;
+}
+//스코프 문제
+export function makeLoop () {
+
+}
+export function objDestructuring () {
+  return;
+}
+export function objInArraySum () {
+    return;
+}
+export function objArrayDestructuringLargest () {
+    return;
+}
+export function objChangeText () {
+    return;
+}
+
+//배열의 2번째 값을 리턴하시오.
+export function secondsVal () {
+  return;
+}
