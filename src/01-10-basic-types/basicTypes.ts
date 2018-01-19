5922f154b7a181f87bc2cf69ab98a04790026a66
rudwnok123 <rudwnok123@naver.com>
2018-01-20 03:00:54 +0900

    1ck

--- src/01-10-basic-types/basicTypes.ts
+++ src/01-10-basic-types/basicTypes.ts
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
-export function makeString(str:string,num:number) {
 
-  return(str + '' + num + ' 반가워');
-=======
-export function makeString() {
-  return;
->>>>>>> 72f645f40db5400c29e6bccd6b9e3aa6059d7cb9
+export function makeString(word:string,num:number) {
+  return(typeof word === 'string' && typeof num === 'number') ? '${word} ${년 반가워}' : '거절한다';
+  //파라미터의 자료형 비교. typescript 에서 비교는 ===, 
+
+export function sum(num1:number , num2:number) {
+  // return(() ? a + b : '두개의 숫자를 넣어주세요';
+  
 }
 
-export function sum() {
-  return;
-}
-
-export function inverseBool() {
-  return;
+export function inverseBool(value : boolean) {
+  return(!value);
 }
 
 export function sumOfAll() {
