e0a98a500cb153950dff4cb3538639ff6458a7da
devjang <devjang2016@gmail.com>
2018-01-22 13:11:29 +0900

    문제의 의도에 대한 주석 추가

--- src/01-20-variable-declarations/variableDeclarations.ts
+++ src/01-20-variable-declarations/variableDeclarations.ts
@@ -1,29 +1,37 @@
-//블록스코스 문제
-export function blockScope () {
-    var a = 40, b =20;
-    if() {
-        var c = a + b;
-    }
-    return c;
+/**
+ * 블록스코프 문제
+ * var를 let으로 대체하여 호이스팅의 문제점을 let으로 어떻게 해결하는지가 문제입니다
+ */
+export function blockScope() {
+  var a = 40, b = 20;
+  if () {
+    var c = a + b;
+  }
+  return c;
 }
-//스코프 문제
-export function makeLoop () {
+
+// 스코프 문제
+export function makeLoop() {
 
 }
-export function objDestructuring () {
+
+export function objDestructuring() {
   return;
 }
-export function objInArraySum () {
-    return;
+
+export function objInArraySum() {
+  return;
 }
-export function objArrayDestructuringLargest () {
-    return;
+
+export function objArrayDestructuringLargest() {
+  return;
 }
-export function objChangeText () {
-    return;
+
+export function objChangeText() {
+  return;
 }
 
 //배열의 2번째 값을 리턴하시오.
-export function secondsVal () {
+export function secondsVal() {
   return;
 }
