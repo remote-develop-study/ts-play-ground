af930983d63d9246ac1eaaf62f17dd31ff6ab5cf
Jang Hyeon-seok <devjang2016@gmail.com>
2018-01-28 18:53:03 +0900

    solve variavle Declarations

--- src/01-20-variable-declarations/variableDeclarations.ts
+++ src/01-20-variable-declarations/variableDeclarations.ts
@@ -2,17 +2,27 @@
  * 블록스코프 문제
  * var를 let으로 대체하여 호이스팅의 문제점을 let으로 어떻게 해결하는지가 문제입니다
  */
-export function blockScope() {
-  var a = 40, b = 20;
-  if (true) {
-    var c = a + b;
+export function blockScope(flag: boolean): number | undefined {
+  let a = 40, b = 20;
+
+  if (flag) {
+    let c = a + b;
+
+    return c;
   }
-  return c;
+  
+  return flag || undefined;
 }
 
 // 스코프 문제
-export function makeLoop() {
-  return;
+export function makeLoop(): string {
+  let result: string = '';
+  
+  for (let i = 0; i < 5; i++) {
+    result += '1';
+  }
+
+  return result;
 }
 
 interface obj {
@@ -22,23 +32,25 @@ interface obj {
   age: number[]
 }
 
-export function getColorByIndex() {
-  return;
+export function getColorByIndex({ color }: obj, index: number): string {
+  return color.length <= index ? 'index out of range' : color[index];
 }
 
-export function getSumOfNumArray() {
-  return;
+export function getSumOfNumArray({ number }: obj): number {
+  return number.reduce((prev, curr) => prev + curr);
 }
 
-export function getOldestAge() {
-  return;
+export function getOldestAge({ age }: obj): number {
+  return Math.max(...age);
 }
 
-export function getChangedText() {
-  return;
+export function getChangedText({ goods }: obj): string | false {
+  return goods.includes('Air') && goods.replace('Air', 'Pro');
 }
 
 //배열의 2번째 값을 리턴하시오.
-export function getSecondValueOfArray() {
-  return;
+export function getSecondValueOfArray(arr: Array<number>): number {
+  const [, second] = arr;
+
+  return second;
 }
