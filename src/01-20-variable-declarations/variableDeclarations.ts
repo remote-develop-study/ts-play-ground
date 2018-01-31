61f9f635f0f549e815cd76040cdbed381a17d453
rudwnok123 <rudwnok123@naver.com>
2018-01-31 23:00:06 +0900

    1차. 한번더 보기

--- src/01-20-variable-declarations/variableDeclarations.ts
+++ src/01-20-variable-declarations/variableDeclarations.ts
@@ -2,17 +2,23 @@
  * 블록스코프 문제
  * var를 let으로 대체하여 호이스팅의 문제점을 let으로 어떻게 해결하는지가 문제입니다
  */
-export function blockScope() {
+export function blockScope(value:number) {
   var a = 40, b = 20;
-  if (true) {
+  if (value) {
     var c = a + b;
+    return c;
   }
-  return c;
+    // var d = false;
+    return undefined;
 }
 
 // 스코프 문제
-export function makeLoop() {
-  return;
+export function makeLoop(num:string) {
+  var num = '';
+  for (var i=0; i<5; i++) {
+    num += '1';
+  }
+  return num;
 }
 
 interface obj {
@@ -22,19 +28,27 @@ interface obj {
   age: number[]
 }
 
-export function getColorByIndex() {
+// 배열의 index값
+export function getColorByIndex({color}:obj,index:number) {
+  return color[index];
+}
+
+// 이렇게 해도되나..
+export function getSumOfNumArray({number}:obj) {
+  var sum = number.reduce((a,b)=>a+b)
+  return sum;
+}
+
+// *---여기서부터 풀기
+//최대값 찾기
+export function getOldestAge({age}:obj) {
+  // var maxNum = age.reduce
   return;
 }
 
-export function getSumOfNumArray() {
-  return;
-}
-
-export function getOldestAge() {
-  return;
-}
-
-export function getChangedText() {
+// 배열값 바꾸기
+export function getChangedText({goods}:obj) {
+  // let change = {};
   return;
 }
 
