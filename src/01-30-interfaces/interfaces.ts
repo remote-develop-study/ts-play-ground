dbf8d85e9acd9c430055f37ec23e0b7dd5d8485d
kyoong <descobrido@gmail.com>
2018-02-03 19:01:30 +0900

    Third sprint complete

--- src/01-30-interfaces/interfaces.ts
+++ src/01-30-interfaces/interfaces.ts
@@ -1,34 +1,76 @@
+interface SquareFunc {
+  (num: number): number
+}
+
+const squarefunc = (num: number) => {
+  return num*num
+}
+
 interface IUser {
-  
+  id: string,
+  pw: string,
+  job?: string
 }
 
-const getUserId = () => {
-
+const getUserId = (user: IUser) => {
+  return user.id
 }
 
-const getUserJob = () => {
-  
+const getUserJob = (user: IUser) => {
+  return user.job || '백수'
 }
 
 interface ISortUser {
-
+  (user: IUser): Array<IUser>
 }
 
-const sortUser = () =>
+const sortUser = (user: Array<IUser>) => {
+  user.sort((a, b) => {
+
+    if (a.id.toUpperCase() > b.id.toUpperCase()) {
+      return 1;
+    }
+
+    if (a.id.toUpperCase() < b.id.toUpperCase()) {
+      return -1;
+    }
+    
+    return 0;
+  })
+  return user
+}
 
 interface IHuman {
-  
+  name: string,
+  borned: number,
+  getAge(): number,
 }
 
-class Human {
-  
+class Human implements IHuman {
+  name: string;
+  borned: number;
+
+  constructor(name: string, borned: number) {
+    this.name = name;
+    this.borned = borned;
+  }
+
+  getAge(): number {
+    return (new Date().getFullYear()) - this.borned;
+  }
 }
 
-const getName = () => 
-const getBorned = () => 
+const getName = (human: IHuman) => {
+  return human.name
+}
+const getBorned = (human: IHuman) => {
+  return human.borned
+}
 
 
 export {
+  SquareFunc,
+  squarefunc,
   IUser,
   getUserId,
   getUserJob,
