f83c278f45663fe6e8f007c486c78c06a6501720
kimyeaseul <adgjl3151@hanmail.net>
2018-02-12 23:32:30 +0900

    변수 문제 품

--- src/01-10-basic-types/basicTypes.ts
+++ src/01-10-basic-types/basicTypes.ts
@@ -1,29 +1,65 @@
-export function makeString(greeting: string, year: number) {
-  return ((typeof greeting !== 'string' || typeof year !== 'number')
-            ? '거절한다'
-            : `${greeting} ${year}년 반가워`);
+// export function makeString(greeting: string, year: number) {
+//   return ((typeof greeting !== 'string' || typeof year !== 'number')
+//             ? '거절한다'
+//             : `${greeting} ${year}년 반가워`);
+// }
+
+// export function sum(num1: number, num2: number) {
+//   return ((!num1 || !num2)
+//             ? '두개의 숫자를 넣어주세요'
+//             : num1 + num2);
+// }
+
+// export function inverseBool(flag: boolean) {
+//   return !flag;
+// }
+
+// export function sumOfAll(arr: Array<number>) {
+//   return arr.reduce((prev, curr) => prev + curr);
+// }
+
+// export function testTuple(x: [string, boolean, number]) {
+//   return x.join(' ');
+// }
+
+// export function testEnum(color: Color) {
+//   const colorName: string = color.Green;
+
+//   return colorName;
+// }
+// 위에는 정답. 아래는 self-solve
+export function makeString(str: string, year: number) {
+  let sentence: string;
+  if(typeof str !== 'string' && typeof year !== 'number'){
+    sentence = '거절한다';
+  }else{
+    sentence = str+ ' '+year+'년 반가워';
+  }
+  return sentence;
 }
 
 export function sum(num1: number, num2: number) {
-  return ((!num1 || !num2)
-            ? '두개의 숫자를 넣어주세요'
-            : num1 + num2);
+  if(!num1 || !num2){
+    return '두개의 숫자를 넣어주세요';
+  }else{
+    return num1+num2;
+  }
 }
 
-export function inverseBool(flag: boolean) {
-  return !flag;
+export function inverseBool(bool: boolean) {
+  return !bool;
 }
 
 export function sumOfAll(arr: Array<number>) {
   return arr.reduce((prev, curr) => prev + curr);
 }
 
-export function testTuple(x: [string, boolean, number]) {
+export function testTuple(x:[string, booolean, number]) {
   return x.join(' ');
 }
 
 export function testEnum(color: Color) {
-  const colorName: string = color.Green;
 
-  return colorName;
+  return color;
 }
+
