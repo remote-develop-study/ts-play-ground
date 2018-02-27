1b52f1161139281201739c67553ef3dbe8543f70
rudwnok123 <rudwnok123@naver.com>
2018-02-27 22:28:45 +0900

    인터페이스 완?

--- src/01-30-interfaces/interfaces.ts
+++ src/01-30-interfaces/interfaces.ts
@@ -1,27 +1,27 @@
 interface SquareFunc {
-  (num:number):number;
+  (num: number): number;
 }
 
-const squarefunc = (num:number) => {
-  return num*num;
+const squarefunc = (num: number) => {
+  return num * num;
 }
 
 interface IUser {
-  id:string;
-  pw:string;
-  job?:string;
+  id: string;
+  pw: string;
+  job?: string;
 }
 
 
 // 여기서부터 수정하기
-const getUserId = (id:IUser,pw:IUser) => {
-  let user = {id : 'dev',pw: 'abc'}
+const getUserId = (id: IUser, pw: IUser) => {
+  let user = { id: 'dev', pw: 'abc' }
   return user.pw;
 }
 
 // ???
-const getUserJob = (id:IUser,pw:IUser,job?:IUser) => {
-  
+const getUserJob = ( job?: IUser) => {
+ return job || '백수'
 }
 
 
@@ -35,23 +35,23 @@ const sortUser = (user: Array<IUser>) => {
     if (a.id.toUpperCase() < b.id.toUpperCase()) {
       return -1;
     }
-    
+
     return 0;
   })
   return user
 }
 
 interface IHuman {
-name:string;
-borned:number;
-getAge():number;
+  name: string;
+  borned: number;
+  getAge(): number;
 }
 
 //완
-class Human implements IHuman{
-  name:string;
-  borned:number;
-  constructor(name: string, borned:number){
+class Human implements IHuman {
+  name: string;
+  borned: number;
+  constructor(name: string, borned: number) {
     this.name = name;
     this.borned = borned;
   }
