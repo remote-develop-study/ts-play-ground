dbde26c5e79f799aac00e2b1361d21fad78df45c
정주영 <nwwn1004@naver.com>
2018-03-12 03:19:57 +0900

    01기본타입

--- src/01-10-basic-types/basicTypes.ts
+++ src/01-10-basic-types/basicTypes.ts
@@ -1,23 +1,27 @@
-export function makeString() {
-  return;
+import { log } from "util";
+
+export function makeString(message:string, year:number):string {
+  return (typeof message !== 'string' || typeof year !== 'number') ? '거절한다' : `${message} ${year}년 반가워`;
 }
 
-export function sum() {
-  return;
+export function sum(num1: number, num2:number): number | string{
+  return arguments.length  !== 0 ? num1 + num2 : '두개의 숫자를 넣어주세요';
 }
 
-export function inverseBool() {
-  return;
+export function inverseBool(bool:boolean): boolean {
+  return !bool;
 }
 
-export function sumOfAll() {
-  return;
+export function sumOfAll(arr:Array<number>): number {
+  return arr.reduce(function(total,num){
+    return total+ num;
+  })
 }
 
-export function testTuple() {
-  return;
+export function testTuple(a:[string,boolean,number]): string {
+  return a.join(' ');
 }
 
-export function testEnum() {
-  return;
+export function testEnum(color: Color): number {
+  return color;
 }
