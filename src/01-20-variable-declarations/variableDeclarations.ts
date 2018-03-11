5e3b57bf53b828c185a15523bb0f987f2962da41
정주영 <nwwn1004@naver.com>
2018-03-12 03:01:56 +0900

    02 변수선언 연습 및 공부

--- src/01-20-variable-declarations/variableDeclarations.ts
+++ src/01-20-variable-declarations/variableDeclarations.ts
@@ -2,21 +2,20 @@
  * 블록스코프 문제
  * var를 let으로 대체하여 호이스팅의 문제점을 let으로 어떻게 해결하는지가 문제입니다
  */
-export function blockScope(logicalValue: boolean): number | undefined {
+export function blockScope(flag: boolean): number | undefined {
   let a = 40, b = 20;
-  if (logicalValue) {
-    let c: number = a + b;
+  if (flag) {
+    let c = a + b;
     return c
   }
   return undefined;
 }
 
 // 스코프 문제
-export function makeLoop() {
-  let i: number = 5;
-  let result: string = '';
-  while (i--) {
-    result += '1'
+export function makeLoop(): string {
+  let result:string ='';
+  for(let i = 0; i < 5; i++){
+    result+='1';
   }
   return result;
 }
@@ -27,34 +26,30 @@ interface obj {
   goods: string,
   age: number[]
 }
-
-export function getColorByIndex(obj: object, idx: number): string | boolean {
-  let { color } = obj;
-  let length: number = color.length;
-  if (idx < length) {
-    return color[idx];
-  } else {
-    return 'index out of range';
-  }
+let obj: obj = {
+  color: ["red", "blue", "green", "pink", "white"],
+  number: [1, 3, 5, 7],
+  goods: "MacBook Air",
+  age: [60, 3, 5, 15, 30, 6]
 }
 
-export function getSumOfNumArray(obj: obj): number {
-  let { number } = obj;
-  return number.reduce((prev: number , cur: number) => prev + cur);
+export function getColorByIndex({ color }: obj, index: number): string {
+  return (index >= 0 && index < color.length) ? color[index] : "index out of range"
+}
+export function getSumOfNumArray({number}: obj): number {
+  // let result:number;
+  return number.reduce((prev, curt) => prev + curt);
 }
 
-export function getOldestAge(obj: obj): number {
-  let { age } = obj;
-  return age.reduce((prev, cur) => prev > cur ? prev : cur)
+export function getOldestAge({age}: obj): number {
+  return Math.max(...age);
 }
 
-export function getChangedText(obj: obj): string {
-  let { goods } = obj;
-  return goods.replace('Air', 'Pro');
+export function getChangedText({goods}: obj): string {
+  return goods.replace('Air','Pro');
 }
 
 //배열의 2번째 값을 리턴하시오.
-export function getSecondValueOfArray(numbers: number[]): number {
-  let [first, second, ...rest] = numbers
-  return second;
+export function getSecondValueOfArray([, second] = {number} ): number {
+  return second
 }
