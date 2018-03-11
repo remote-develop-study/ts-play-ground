08f6e60ecf9acf7cd58be2f16b215790ef52c15e
rudwnok123 <rudwnok123@naver.com>
2018-03-11 20:49:52 +0900

    수정 테스트

--- src/vending-machine/vendingMachine.ts
+++ src/vending-machine/vendingMachine.ts
@@ -1,6 +1,22 @@
-const drinks = [
-  {
-    name: 'cola',
-    price: 1500,
+// 자판기 
+
+
+class vendingMachine {
+  constructor() {
+
   }
-]
+  coin: number;
+  result: number;
+  drink1 = 1000;
+  // drink = [];
+  // constructor(){
+  //   this.coin = 0;
+  // }
+  // coinResult = 0;
+
+  coinInsert(coin: number) {
+    let coinResult = coin - this.drink1;
+    return coinResult;
+  }
+}
+
