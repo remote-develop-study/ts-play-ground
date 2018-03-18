f65e2637a332bceeb779050a31f59113f1dee06f
kyoong <descobrido@gmail.com>
2018-03-19 00:42:06 +0900

    Add exclamation mark for not undefined value

--- src/vending-machine/src/vending-machine.ts
+++ src/vending-machine/src/vending-machine.ts
@@ -14,7 +14,7 @@ interface VendingMachine {
   insertedCoin: number;
   giveChange(): void;
   giveProduct(productName: string): DrinkOrSnack | void;
-  getProductByName(productName: string): DrinkOrSnack;
+  getProductByName(productName: string): DrinkOrSnack | undefined;
   insertCoin(coin: number): void;
   reduceProduct?(productName: string): void;
   notEnoughProductError(productName: string): string;
@@ -25,10 +25,12 @@ class AwesomeVendingMachine implements VendingMachine {
   public products: Inventory;
   public insertedCoin: number;
   public drinks: Array<Drinks>;
+  public snack?: Array<Snack>;
 
   constructor(products: Inventory) {
     this.products = products;
     this.drinks = this.products.drinks;
+    this.snack = this.products.snack;
     this.insertedCoin = 0;
   }
 
@@ -37,16 +39,22 @@ class AwesomeVendingMachine implements VendingMachine {
     this.insertedCoin = 0
   }
 
-  public getProductByName(productName: string): Drinks {
-    return <Drinks>this.drinks.filter(value =>  value.name === productName)[0]
+  public getProductByName(productName: string): Drinks | undefined{
+    return this.drinks.find(value => value.name === productName)
   }
 
+  // 상품 이름을 받아서 상품을 반환
+  // 수량 체크, 잔돈 감소, 에러 처리
   public giveProduct(productName: string): Drinks | void {
-    const selected: Drinks = this.getProductByName(productName);
+    const selected: Drinks = this.getProductByName(productName)!;
     const haveProductsInStock: boolean = selected ? selected.quantity > 0 : false;
 
     if (haveProductsInStock) {
       selected.quantity--
+      // 유효성 검사 메소드로 따로 빼는걸 고려
+      // validator
+        // validate.balance
+        // validate.quantity
       if (this.insertedCoin > selected.sellingPrice) {
         this.insertedCoin = this.insertedCoin - selected.sellingPrice
         return selected
@@ -72,7 +80,6 @@ class AwesomeVendingMachine implements VendingMachine {
   // public reduceProduct(drinkName: string): void {
   //   // 이걸 모르게씀
   //   const selected: DrinkOrSnackOrUndefined = this.getProductByName(drinkName)
-
   // }
 
   public notEnoughProductError(productName: string): string {
