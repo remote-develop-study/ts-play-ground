dbb70d1ddd0edbfb690c6eaf674c8c5217aeefe2
rudwnok123 <rudwnok123@naver.com>
2018-03-24 20:41:48 +0900

    webpack-test

--- src/03-19-count-app/webpack.config.js
+++ src/03-19-count-app/webpack.config.js
@@ -1,16 +1,32 @@
 // 모듈 정의하고 외부에 배포할 모듈을 전달해 모듈 정의?
 module.exports  = {
   // messeage : 'webpack'
+  
   entry : './count.ts',
   output:{
     path: __dirname + '/public',
     filename:'bundle.js'
   },
   devServer:{
+    hot:true,
     contentBase: __dirname + '/public'
+  },
+  module:{
+    loaders:[
+      {
+        test:/\.ts$/,
+        loader:'babel-loader',
+        exclude:/node_modules/,
+        query:{
+          cacheDirectory: true,
+          presets: ['es2015', 'ts']
+        }
+      }
+    ]
   }
 };
 
 
 
 
+a
