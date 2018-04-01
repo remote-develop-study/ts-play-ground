35af140ced3578e19d491c986e2a727d906b9ecd
kyoong <descobrido@gmail.com>
2018-04-01 22:03:34 +0900

    Polygon Complete

---
+++ src/03-26-polygon/shaep-area-calculator.js
@@ -0,0 +1,55 @@
+var hasNegativeNumber = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.filter(function (value) { return value < 0; }).length > 0;
+};
+var MyShapeAreaCalculator = /** @class */ (function () {
+    function MyShapeAreaCalculator() {
+    }
+    MyShapeAreaCalculator.prototype.circle = function (radius) {
+        return Math.sqrt(radius) * Math.PI;
+    };
+    MyShapeAreaCalculator.prototype.rect = function (wide, long) {
+        return wide * long;
+    };
+    MyShapeAreaCalculator.prototype.trapezoid = function (height, base1, base2) {
+        return ((base1 + base2) / 2) * height;
+    };
+    MyShapeAreaCalculator.prototype.calculateCircle = function (radius) {
+        if (hasNegativeNumber(radius))
+            throw new Error("반지름은 0 보다 커야 합니다");
+        return this.circle(radius);
+    };
+    MyShapeAreaCalculator.prototype.calculateRect = function (wide, long) {
+        if (hasNegativeNumber(wide, long))
+            throw new Error("가로, 세로의 길이는 0 보다 커야 합니다");
+        return this.rect(wide, long);
+    };
+    MyShapeAreaCalculator.prototype.calculateTrapezoid = function (height, base1, base2) {
+        if (hasNegativeNumber(height, base1, base2))
+            throw new Error("높이나 각 변의 길이는 모두 0 보다 커야 합니다.");
+        return this.trapezoid(height, base1, base2);
+    };
+    MyShapeAreaCalculator.prototype.getArea = function (shapeName) {
+        var args = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            args[_i - 1] = arguments[_i];
+        }
+        switch (shapeName) {
+            case 'circle':
+                var radius = args[0];
+                return this.calculateCircle(radius);
+            case 'rect':
+                var wide = args[0], long = args[1];
+                return this.calculateRect(wide, long);
+            case 'trapezoid':
+                var height = args[0], base1 = args[1], base2 = args[2];
+                return this.calculateTrapezoid(height, base1, base2);
+            default:
+                return 0;
+        }
+    };
+    return MyShapeAreaCalculator;
+}());
---
+++ src/03-26-polygon/shaep-area-calculator.ts
@@ -0,0 +1,52 @@
+interface ShapeAreaCalculator {
+  circle(radius: number): number;
+  rect(wide: number, long: number): number;
+  trapezoid(height: number, base1: number, base2: number): number;
+  calculateCircle(radius: number): number;
+  calculateRect(wide: number, long: number): number;
+  calculateTrapezoid(height: number, base1: number, base2: number): number;
+}
+
+const hasNegativeNumber = (...numbers: number[]): boolean => {
+  return numbers.filter((value) => { return value < 0 }).length > 0
+}
+
+class MyShapeAreaCalculator implements ShapeAreaCalculator {
+  
+  public circle(radius: number) {
+    return Math.sqrt(radius) * Math.PI
+  }
+  public rect(wide: number, long: number) {
+    return wide * long
+  }
+  public trapezoid(height: number, base1: number, base2: number) {
+    return ((base1+base2)/2)*height
+  }
+  public calculateCircle(radius: number) {
+    if (hasNegativeNumber(radius)) throw new Error("반지름은 0 보다 커야 합니다");
+    return this.circle(radius)
+  }
+  public calculateRect(wide: number, long: number) {
+    if (hasNegativeNumber(wide, long)) throw new Error("가로, 세로의 길이는 0 보다 커야 합니다");
+    return this.rect(wide, long)
+  }
+  public calculateTrapezoid(height: number, base1: number, base2: number) {
+    if (hasNegativeNumber(height, base1, base2)) throw new Error("높이나 각 변의 길이는 모두 0 보다 커야 합니다.")
+    return this.trapezoid(height, base1, base2)
+  }
+  public getArea(shapeName: 'circle' | 'rect' | 'trapezoid', ...args: number[]) {
+    switch (shapeName) {
+      case 'circle':
+        const [radius] = args
+        return this.calculateCircle(radius)
+      case 'rect':
+        const [wide, long] = args
+        return this.calculateRect(wide, long)
+      case 'trapezoid':
+        const [height, base1, base2] = args
+        return this.calculateTrapezoid(height, base1, base2)
+      default:
+        return 0;
+    }
+  }
+}
