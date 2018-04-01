e5e9ac9649f04de9a6ffda3f6fd96ddee450ef50
kyoong <descobrido@gmail.com>
2018-04-02 00:19:31 +0900

    Complete Calculator @0402

---
+++ src/04-02-calculator/calculator.ts
@@ -0,0 +1,54 @@
+interface Calculator {
+  add(a: number): void
+  sub(a: number): void
+  div(a: number): void
+  mul(a: number): void
+  display(): void
+}
+
+
+class MyCalculator implements Calculator {
+  
+  private _current: number;
+
+  constructor(public initNumber: number) {
+    this.current = 0
+  }
+
+  add(a: number): MyCalculator {
+    this.current += a
+    return this
+  }
+  sub(a: number): MyCalculator {
+    this.current -= a
+    return this
+  }
+  div(a: number): MyCalculator {
+    this.current /= a
+    return this
+  }
+  mul(a: number): MyCalculator {
+    this.current *= a
+    return this
+  }
+
+  get current (): number {
+    return this._current
+  }
+
+  set current (result: number)  {
+    this._current = result
+  }
+  
+  display(decimal: number = 2): void {
+    console.log(this.current.toFixed(decimal))
+  }
+
+  allClear(): void {
+    this.current = 0
+  }
+
+}
+
+// Test
+new MyCalculator(3).add(5).mul(5).div(3).div(3).display() //2.777
