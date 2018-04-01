dcd932db6f541fe2ac025f9424ecabebee615313
rudwnok123 <rudwnok123@naver.com>
2018-04-01 23:43:58 +0900

    class->function화

--- src/03-26-polygon/polygon.js
+++ src/03-26-polygon/polygon.js
@@ -1,73 +1,37 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Polygon = /** @class */ (function () {
-    function Polygon() {
+"use strict";
+// interface Polygon {
+//   circle();  
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// *---간단?
+var pi = 3.14;
+// 원
+function circle(r) {
+    return r * r * pi;
+}
+exports.circle = circle;
+function rect(a, b) {
+    return a * b;
+}
+exports.rect = rect;
+function trapezoid(a, b, h) {
+    return (a + b) * 0.5 * h;
+}
+exports.trapezoid = trapezoid;
+// function calculateCircle(r: number) {
+//   circle(3);
+// }
+function getArea(type, num1, num2, num3) {
+    // 2번문제의 타입체크가하기
+    // 3번문제 공통으로 묶기?
+    if (type == "circle") {
+        circle(3);
     }
-    return Polygon;
-}());
-// *---- 원 넓이 구하기
-var Circle = /** @class */ (function (_super) {
-    __extends(Circle, _super);
-    function Circle(num) {
-        var _this = _super.call(this) || this;
-        _this.pi = 3.14;
-        _this.num = num;
-        return _this;
+    if (type == "rect") {
+        rect(2, 4);
     }
-    Circle.prototype.getArea = function () {
-        //  this.pi
-        return this.num * this.num * this.pi;
-    };
-    return Circle;
-}(Polygon));
-var circle = new Circle(3);
-console.log('원의 넓이 ' + circle.getArea());
-// 공식이 여러가지가 있던데...
-var Rect = /** @class */ (function (_super) {
-    __extends(Rect, _super);
-    function Rect(line1, line2, line3, line4) {
-        var _this = _super.call(this) || this;
-        // this.diagonal1 = diagonal1;
-        _this.line1 = line1;
-        _this.line2 = line2;
-        _this.line3 = line3;
-        _this.line4 = line4;
-        return _this;
+    if (type == "trapezoid") {
+        trapezoid(2, 4, 5);
     }
-    //가로지르는 대각선의 길이를 알 경우
-    // getArea() {
-    // }
-    // 4개의 각 변의 길이를 알 경우
-    Rect.prototype.getArea = function () {
-        return (this.line1 * this.line2) / 2 + (this.line3 * this.line4) / 2;
-    };
-    return Rect;
-}(Polygon));
-var rect = new Rect(2, 2, 2, 2);
-console.log('사각형의 넓이 ' + rect.getArea());
-// *---- 사다리꼴 넓이 구하기
-var Trapezoid = /** @class */ (function (_super) {
-    __extends(Trapezoid, _super);
-    function Trapezoid(topBr, bottomBr, height) {
-        var _this = _super.call(this) || this;
-        _this.topBr = topBr;
-        _this.bottomBr = bottomBr;
-        _this.height = height;
-        return _this;
-    }
-    Trapezoid.prototype.trapezoidArea = function () {
-        return;
-    };
-    return Trapezoid;
-}(Polygon));
-var trapezoid = new Trapezoid(4, 4, 5);
-console.log('사다리꼴 넓이 ' + trapezoid.trapezoidArea());
+}
 //# sourceMappingURL=polygon.js.map
--- src/03-26-polygon/polygon.ts
+++ src/03-26-polygon/polygon.ts
@@ -1,71 +1,40 @@
-interface polygon {
-}
-class Polygon {
+// interface Polygon {
+//   circle();  
+// }
+
+// *---간단?
+var pi: number = 3.14;
+
+// 원
+export function circle(r: number): number {
+  return r * r * pi;
 }
 
-// *---- 원 넓이 구하기
-class Circle extends Polygon{
-  pi: number = 3.14;
-  num : number;
-  constructor(num: number) {
-    super();
-      this.num = num;
-  }
-  getArea() {
-    //  this.pi
-      return this.num*this.num*this.pi;
-  }
+export function rect(a: number, b: number): number {
+  return a * b;
 }
-let circle: Circle = new Circle(3);
-console.log('원의 넓이 '+circle.getArea());
 
-// 공식이 여러가지가 있던데...
-class Rect extends Polygon {
-  diagonal1: number;
-  diagonal2: number;
-  line1:number;
-  line2:number;
-  line3:number;
-  line4:number;
-  constructor(line1:number,line2:number,line3:number,line4:number){
-    super();
-    // this.diagonal1 = diagonal1;
-    this.line1 = line1;
-    this.line2 = line2;
-    this.line3 = line3;
-    this.line4 = line4
+export function trapezoid(a: number, b: number, h: number) {
+  return (a + b) * 0.5 * h;
+}
+
+// function calculateCircle(r: number) {
+//   circle(3);
+// }
+
+function getArea(type:string, num1:number, num2:number, num3:number) {
+  // 2번문제의 타입체크가하기
+
+  // 3번문제 공통으로 묶기?
+  if(type == "circle"){
+    circle(3);
   }
-  //가로지르는 대각선의 길이를 알 경우
-  // getArea() {
-
-  // }
-  // 4개의 각 변의 길이를 알 경우
-  getArea():number{
-    return (this.line1*this.line2)/2 + (this.line3*this.line4)/2
+  if(type == "rect"){
+    rect(2,4);
+  }
+  if(type == "trapezoid"){
+    trapezoid(2,4,5);
   }
 }
-let rect: Rect = new Rect(2,2,2,2);
-console.log('사각형의 넓이 '+rect.getArea());
-
-
-// *---- 사다리꼴 넓이 구하기
-class Trapezoid extends Polygon {
-  private topBr: number;
-  private bottomBr: number;
-  private height: number;
-  constructor(topBr:number, bottomBr:number, height:number){
-    super();
-    this.topBr = topBr;
-    this.bottomBr = bottomBr;
-    this.height = height;
-  }
-
-  trapezoidArea():number {
-    return 
-  }
-}
-let trapezoid: Trapezoid = new Trapezoid(4,4,5);
-console.log('사다리꼴 넓이 '+trapezoid.trapezoidArea());
-
 
 
