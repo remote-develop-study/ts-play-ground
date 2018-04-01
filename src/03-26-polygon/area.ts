2bbdd766922ee078d48d24a463240cecc8be3ba6
kimyeaseul <adgjl3151@hanmail.net>
2018-04-01 18:24:38 +0900

    getArea 함수 에러처리 추가

--- src/03-26-polygon/area.ts
+++ src/03-26-polygon/area.ts
@@ -26,22 +26,42 @@ function calculateCircle(value:any):string{ //?역할
     return msg;
 }
 
+
 function getArea(figure:string, val1:number, val2:number = 0, val3:number = 0):string{
 
-    
-    let area;
-    let msg;
-    if(figure === 'circle'){
-        msg = 'figure is \' '+ figure +'\', radius is ' + val1 ;
-        area = Math.pow(val1, 2) * Math.PI;
-    }else if(figure === 'rect'){
-        msg = 'figure is \' '+ figure +'\', horizontal is ' + val1 + ', vertical is '+ val2;
-        area = val1 * val2;
-    }else if(figure === 'trapezoid'){
-        msg = 'figure is \' '+ figure +'\', upperSide is ' + val1 + ', lowerSide is ' +val2 +', height is ' + val3 ;
-        area = (val1 + val2) * val3 / 2 ;
+    let area:number = 0;
+    let msg:string = '';
+
+    if(figure == null){
+        msg = "넓이를 구할 도형을 첫번째 인자로 적어주세요(circle / rect / trapezoid)";
+    }else if( typeof(figure) !== 'string' ){
+        msg =  "첫번째 인자는 문자형입니다.";
+    }else if( val1 == null ){
+        msg =  "최소 한가지 이상 값이 필요합니다. "; 
+    }else if( typeof(val1) !== 'number' ){
+        msg =  "두번째 인자부터는 숫자형입니다.";
+    }else if(val1 <= 0 ){
+        msg =  "숫자는 0 보다 커야합니다. "; 
     }else{
 
+        if(figure === 'circle'){
+            msg = 'figure is \' '+ figure +'\', radius is ' + val1 +', area is ';
+            area = Math.pow(val1, 2) * Math.PI;
+        }else if(figure === 'rect'){
+            if(val2 <=0){
+                msg = "숫자는 2개 이상 양수로 입력해주세요.";
+            }else{
+                msg = 'figure is \' '+ figure +'\', horizontal is ' + val1 + ', vertical is '+ val2+', area is ';
+                area = val1 * val2;
+            }
+        }else if(figure === 'trapezoid'){
+            if(val2 <= 0 || val3 <= 0 ){
+                msg = "숫자를 3개 이상 양수로 입력해 주세요.";
+            }else{
+                msg = 'figure is \' '+ figure +'\', upperSide is ' + val1 + ', lowerSide is ' +val2 +', height is ' + val3 +', area is ';
+                area = (val1 + val2) * val3 / 2 ;
+            }
+        }
     }
-    return msg + ' ' + figure + '의 넓이는 ' + area + '입니다. ';
+    return area ? msg + area : msg ;
 }
