7ee60bd605fa0495910a08155d3b6a2dc746b0dc
정주영 <nwwn1004@naver.com>
2018-04-01 19:30:47 +0900

    03-26-polygon

--- src/03-26-polygon/polygon.spec.ts
+++ src/03-26-polygon/polygon.spec.ts
@@ -1,4 +1,4 @@
-import { circle, rect, trapezoid } from './polygon';
+import { circle, rect, trapezoid, calculateCircle, getArea } from './polygon';
 import { } from 'jest'
 
 describe('다각형 구하기', function () {
@@ -12,5 +12,29 @@ describe('다각형 구하기', function () {
   it('아랫변,윗변,높이 값을 받아 사다리꼴의 넓이를 구한다.',function(){
       expect(trapezoid(8,4,4)).toBe(24);
   })
-
 });
+
+describe('파라미터를 체크하는 다각형의 넓이 함수',function(){
+  it('숫자형타입만 계산이 가능합니다.',function(){
+    expect(calculateCircle("10")).toBe("숫자형타입만 계산이 가능합니다.");
+  })
+  it('최소 한가지 값이 필요합니다.',function(){
+    expect(calculateCircle()).toBe("최소 한가지 값이 필요합니다.");
+  })
+  it('반지름은 0보다 커야 합니다.',function(){
+    expect(calculateCircle(-1)).toBe("반지름은 0보다 커야 합니다.");
+  })
+  
+});
+
+describe('모든 넓이 값을 얻을 수 있는 getArea 함수',function(){
+  it('원의 넓이 값 출력',function(){
+    expect(getArea('circle',10)).toBe(314);
+  })
+  it('사각형의 넓이 값 출력',function(){
+    expect(getArea('rect',10,15)).toBe(150);
+  })
+  it('사다리꼴의 넓이 값 출력',function(){
+    expect(getArea('trapezoid',10, 15, 12)).toBe(150);
+  })
+})
--- src/03-26-polygon/polygon.ts
+++ src/03-26-polygon/polygon.ts
@@ -2,9 +2,40 @@ const PI:number = 3.14;
 export function circle(radius: number): number {
     return radius * radius * PI;
 }
+
 export function rect(horizontal: number, vertical: number): number {
     return horizontal * vertical
 }
+
 export function trapezoid(below: number, upper: number, height: number): number{
     return (below + upper) * height/2
+}
+
+export function calculateCircle(radius){
+    if(typeof radius == String ) {
+        return '숫자형타입만 계산이 가능합니다.'
+    }
+
+    if(radius == undefined) {
+        return '최소 한가지 값이 필요합니다.'
+    }
+
+    if(radius < 0) {
+        return '반지름은 0보다 커야 합니다.'
+    }
+}
+
+export function getArea(fcname: string, param1: number, param2: number, param3: number){
+    console.log(fcname);
+    switch(fcname){
+        case 'circle':
+            return circle(param1);
+            break;
+        case 'rect':
+            return rect(param1, param2);
+            break;
+        case 'trapezoid':
+            return trapezoid(param1, param2, param3);
+            break;
+    }
 }
