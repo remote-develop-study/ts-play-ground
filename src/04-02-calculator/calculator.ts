e91507c2f9594cd097c621ea3f58925883336f80
obeythecorgi <descobrido@gmail.com>
2018-04-11 20:36:20 +0900

    Merge master into branch

--- src/04-02-calculator/calculator.ts
+++ src/04-02-calculator/calculator.ts
@@ -1,11 +1,11 @@
 interface Calculator {
-  add(a: number): Calculator
-  sub(a: number): Calculator
-  div(a: number): Calculator
-  mul(a: number): Calculator
-  done(decimalToDisplay?: number): Calculator
-  allClear(): Calculator
-  reset(value: number): Calculator
+  add(a: number): this
+  sub(a: number): this
+  div(a: number): this
+  mul(a: number): this
+  done(decimalToDisplay?: number): this
+  allClear(): this
+  reset(value: number): this
 }
 
 
@@ -18,24 +18,24 @@ class MyCalculator implements Calculator {
     this._expr = ''
   }
 
-  add(a: number): Calculator {
+  add(a: number): this {
     this.expr += `+${a.toString()}`
     return this
   }
-  sub(a: number): Calculator {
+  sub(a: number): this {
     this.expr += `-${a.toString()}`
     return this
   }
-  div(a: number): Calculator {
+  div(a: number): this {
     this.expr += `/${a.toString()}`
     return this
   }
-  mul(a: number): Calculator {
+  mul(a: number): this {
     this.expr += `*${a.toString()}`
     return this
   }
 
-  done(decimalToDisplay: number = 2): Calculator {
+  done(decimalToDisplay: number = 2): this {
     console.log(eval(this._initValue + this.expr).toFixed(decimalToDisplay))
     return this
   }
@@ -48,12 +48,12 @@ class MyCalculator implements Calculator {
     this._expr = result
   }
 
-  reset (value: number): Calculator {
+  reset (value: number): this {
     this._initValue = value
     return this
   }
 
-  allClear(): Calculator {
+  allClear(): this {
     this._initValue = 0
     this.expr = ''
     return this
@@ -69,6 +69,6 @@ new MyCalculator(3).add(5).mul(5).div(3).div(3).done()
 
 /** 
  * 5+5*5/5 = 10.00
- * 5+5 = 10.000
+ * 5+5 = 10.0000
 */
 new MyCalculator(5).add(5).mul(5).div(5).done().allClear().reset(5).add(5).done(4) 
