8c7bac465db62104374883a0026774e7d1a245c4
정주영 <nwwn1004@naver.com>
2018-04-22 21:28:22 +0900

    es6AndTs

--- src/04-09-review-and-interface/es6AndTs.spec.ts
+++ src/04-09-review-and-interface/es6AndTs.spec.ts
@@ -1,29 +1,28 @@
 import { double, greet, minNumbers, newArray, testResults, scientist } from './es6AndTS';
 
 describe('TS 스터디 4월 9일 ES6 리뷰', () => {
-  it('화살표 함수 반환 확인', () => {
+  it('1. 화살표 함수 반환 확인', () => {
     expect(double(3)).toBe(6);
   });
 
-  it('기본 매개변수 반환 확인', () => {
+  it('2. 기본 매개변수 반환 확인', () => {
     const name: string = '이름을 넣어주세요';
-
     expect(greet(name)).toBe(name);
   });
 
-  it('기본 매개변수 확인', () => {
+  it('2. 기본 매개변수 확인', () => {
     expect(greet()).toBe('Max');
   });
 
-  it('전개 연산자 확인', () => {
+  it('3. 전개 연산자 확인', () => {
     expect(minNumbers([-3, 33, 38, 5])).toBe(-3);
   });
 
-  it('배열 분해 확인', () => {
+  it('4. 배열 분해 확인', () => {
     expect(testResults([3.89, 2.99, 1.38])).toBe('3.89, 2.99, 1.38');
   });
 
-  it('객체 분해 확인', () => {
+  it('5. 객체 분해 확인', () => {
     expect(scientist({ firstName: 'Will', experience: 12 })).toEqual({
       firstName: 'Will',
       experience: 12,
--- src/04-09-review-and-interface/es6AndTs.ts
+++ src/04-09-review-and-interface/es6AndTs.ts
@@ -1,16 +1,17 @@
 // 100% TS & ES6로 전환 (타입도 빠짐없이 달아주세요)
 
 // 1. 화살표 함수로 바꿔주세요
-var double = function(value) {
+let double = (value:number):number => {
   return value * 2;
 };
 
-// 2. Default Parameters를 사용해주세요
-var greet = function(name) {
+// 2. Default Parameters를 사용해주세요 (기본 매개변수 반환 확인)
+let greet = (name:string = 'jung juyoung'):string => {
   if (name === undefined) {
     name = 'Max';
   }
   console.log('Hello, ' + name);
+  return name;
 };
 
 // 3. (Spread Operator)를 사용하여 minNumbers의 apply를 대체해주세요
@@ -21,19 +22,16 @@ var originArr = [55, 20];
 const newArray = (newArr: number[]): number => Array.prototype.push.apply(originArr, newArr);
 
 // 5. 더 짧게 만들어주세요 (Destructuring Arrays)
-const testResults = function(arr: number[]) {
-  var result1 = arr[0];
-  var result2 = arr[1];
-  var result3 = arr[2];
-
+const testResults = function(arr: number[] = [0,1,2]):string {
+  let [result1, result2, result3] = [...arr]
   return result1 + ', ' + result2 + ', ' + result3;
 };
 
 // 6. 더 짧게 만들어주세요 (Destructuring Objects)
-const scientist = function(obj): Object {
-  var firstName = obj.firstName;
-  var experience = obj.experience;
-
+  const scientist = function(obj): Object {
+  // var firstName = obj.firstName;
+  // var experience = obj.experience;
+  {firstName, experience} = obj;
   return { firstName, experience };
 };
 
