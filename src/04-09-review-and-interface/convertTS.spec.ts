656ae312740a64ebe72f738102fc221ea811b64e
정주영 <nwwn1004@naver.com>
2018-04-22 19:26:11 +0900

    convertTs

--- src/04-09-review-and-interface/convertTS.spec.ts
+++ src/04-09-review-and-interface/convertTS.spec.ts
@@ -4,11 +4,12 @@ describe('TS 스터디 4월 9일 TS 전환', () => {
   it('bankAccount 객체의 money 프로퍼티 타입을 확인합니다', () => {
     expect(typeof mySelf.bankAccount.money).toBe('number');
   });
-
+  it('bankAccount 객체의 getBalance 반환 타입을 확인한다. ', ()=> {
+    expect(typeof mySelf.bankAccount.getBalance()).toBe('number')
+  });
   it('입금 후 잔고를 확인합니다', () => {
     const { bankAccount: myBank } = mySelf;
-    myBank.deposit(3000);
-
-    expect(myBank.getBalance()).toBe(5000);
+    myBank.deposit(10000);
+    expect(myBank.getBalance()).toBe(12000);
   });
 });
--- src/04-09-review-and-interface/convertTS.ts
+++ src/04-09-review-and-interface/convertTS.ts
@@ -1,10 +1,10 @@
 // Type Aliase를 사용해서 빠짐없이 타입을 달아주세요
 let bankAccount = {
   money: 2000,
-  deposit(value) {
+  deposit(value:number) {
     this.money += value;
   },
-  getBalance: function() {
+  getBalance: function():number {
     return this.money;
   }
 };
