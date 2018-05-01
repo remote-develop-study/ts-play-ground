0c407b300ce6ec514621e53e7a4b048be6a8e021
kimyeaseul <adgjl3151@hanmail.net>
2018-05-01 17:41:53 +0900

    update:0501

--- src/04-09-review-and-interface/es6AndTs.ts
+++ src/04-09-review-and-interface/es6AndTs.ts
@@ -1,20 +1,28 @@
 // 100% TS & ES6로 전환 (타입도 빠짐없이 달아주세요)
 
 // 1. 화살표 함수로 바꿔주세요
-var double = function(value) {
-  return value * 2;
-};
+// var double = function(value) {
+//   return value * 2;
+// };
+var double = (value:number) => value*2;
 
 // 2. Default Parameters를 사용해주세요
-var greet = function(name) {
-  if (name === undefined) {
-    name = 'Max';
-  }
-  console.log('Hello, ' + name);
+// var greet = function(name) {
+//   if (name === undefined) {
+//     name = 'Max';
+//   }
+//   console.log('Hello, ' + name);
+// };
+
+let greet = function(name:string = 'Max') {
+  return name;
 };
 
 // 3. (Spread Operator)를 사용하여 minNumbers의 apply를 대체해주세요
-const minNumbers = (arr: number[]): number => Math.min.apply(Math, arr);
+// const minNumbers = (arr: number[]): number => Math.min.apply(Math, arr);
+const minNumbers = (arr:number[]):number => Math.min(...arr);
+//apply() 메소드는 주어진 this값과 arguments로 함수를 호출합니다.
+//arguments에는 배열( 또는 유사배열객체 array-like object)가 올 수 있습니다.
 
 // 4. (Rest Operator)를 사용하여 newArr의 apply를 대체해주세요
 var originArr = [55, 20];
@@ -22,19 +30,19 @@ const newArray = (newArr: number[]): number => Array.prototype.push.apply(origin
 
 // 5. 더 짧게 만들어주세요 (Destructuring Arrays)
 const testResults = function(arr: number[]) {
-  var result1 = arr[0];
-  var result2 = arr[1];
-  var result3 = arr[2];
-
+  // var result1 = arr[0];
+  // var result2 = arr[1];
+  // var result3 = arr[2];
+  let [result1, result2, result3] = arr;
   return result1 + ', ' + result2 + ', ' + result3;
 };
 
 // 6. 더 짧게 만들어주세요 (Destructuring Objects)
 const scientist = function(obj): Object {
-  var firstName = obj.firstName;
-  var experience = obj.experience;
-
-  return { firstName, experience };
+  // var firstName = obj.firstName;
+  // var experience = obj.experience;
+  let firstName,experience;
+  return ({firstName, experience} = { firstName: obj.firstName, experience: obj.experience});
 };
 
 export { double, greet, minNumbers, newArray, testResults, scientist };
