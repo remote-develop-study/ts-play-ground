33dee0a98c60b6388b1c7299f4683209a7e2d159
Chris Yang <ysm0622@gmail.com>
2018-05-04 01:01:56 +0900

    0622: Auto stash before merge of "ysm0622" and "master"

--- src/04-30-class-second-part/rewriteClass.spec.ts
+++ src/04-30-class-second-part/rewriteClass.spec.ts
@@ -1,45 +1,46 @@
-import { Person, Rectangle, Square, parent, child } from './rewriteClass'; 
+import { Person, Rectangle, Square, parent, child } from './rewriteClass'
 
 describe('Exercise 1 - Class', () => {
+  const person = new Person(name);
+  
   it('새로운 인스턴스 프로퍼티 확인', () => {
     const name = '이름을 넣어주세요';
     const person = new Person(name);
 
-    expect(person.greeting()).toBe(`My name is ${name}`);
-  });
+    expect(person.greeting()).toBe(`My name is ${name}`)
+  })
 
   it('비공개 멤버 변수 접근권한 확인', () => {
-    expect(person.age).toBe(30);
-  });
-});
+    expect(person.age).toBe(30)
+  })
+})
 
 describe('Exercise 2 - Inheritance', () => {
   // 밑의 프로토타입 연결은 TS 전환 후 제거해야합니다.
-  Square.prototype = new Rectangle();
-  Square.prototype.constructor = Square;
+  // Square.prototype = new Rectangle();
+  // Square.prototype.constructor = Square;
 
-  const rect = new Rectangle(5, 10);
-  const square = new Square(6);
+  const rect = new Rectangle(5, 10)
+  const square = new Square(6)
 
   it('get super class Rectangle size', () => {
-    expect(rect.getArea()).toBe(50);
-  });
+    expect(rect.getArea()).toBe(50)
+  })
 
   it('get sub class Square size', () => {
-    expect(square.getArea()).toBe(36);
-  });
-});
+    expect(square.getArea()).toBe(36)
+  })
+})
 
 describe('Exercise 3 - Object Inheritance', () => {
   const rect = new Rectangle(5, 10);
   const square = new Square(6);
 
   it('parent.sayName()', () => {
-    expect(parent.sayName()).toBe('Parent');
-  });
+    expect(parent.sayName()).toBe('Parent')
+  })
 
   it('child.sayName()', () => {
-    expect(child.sayName()).toBe('Child');
-  });
-});
-
+    expect(child.sayName()).toBe('Child')
+  })
+})
--- src/04-30-class-second-part/rewriteClass.ts
+++ src/04-30-class-second-part/rewriteClass.ts
@@ -1,43 +1,66 @@
 /**
  * Exercise 1 - Class
- * 
- * @param name 
+ *
+ * @param name
  */
-function Person(name) {
-  // 생성자내에서만 사용가능
-  var age = 30;
-  this.name = name;
-  
-  this.greeting = function() {
-    return 'My name is ' + this.name;
-  };
+// function Person(name) {
+//   // 생성자내에서만 사용가능
+//   var age = 30
+//   this.name = name
+
+//   this.greeting = function() {
+//     return 'My name is ' + this.name
+//   }
+// }
+
+class Person {
+  age: number = 30
+  constructor(public name: string) {}
+  greeting() {
+    return 'My name is ' + this.name
+  }
 }
 
-Person.prototype.greeting = function() {
-  return this.name;
-};
+// Person.prototype.greeting = function() {
+//   return this.name
+// }
 
 /**
  * Exercise 2 - Inheritance
- * 
- * @param length 
- * @param width 
+ *
+ * @param length
+ * @param width
  */
 // 기본 클래스
-function Rectangle(length, width) {
-  this.length = length;
-  this.width = width;
+
+class Rectangle {
+  constructor(public length: number, public width: number) {}
+  getArea() {
+    return this.length * this.width
+  }
 }
 
-Rectangle.prototype.getArea = function() {
-  return this.length * this.width;
+class Square extends Rectangle{
+  constructor(public size: number) {
+    super(size,size)
+  }
 }
 
-// 서브 클래스
-function Square(size) {
-  this.length = size;
-  this.width = size;
-}
+
+// function Rectangle(length, width) {
+//   this.length = length
+//   this.width = width
+// }
+
+// Rectangle.prototype.getArea = function() {
+//   return this.length * this.width
+// }
+
+// // 서브 클래스
+// function Square(size) {
+//   this.length = size
+//   this.width = size
+// }
 
 /**
  * Exercise 3 - Object Inheritance
@@ -45,23 +68,17 @@ function Square(size) {
 var parent = {
   name: 'Parent',
   sayName: function() {
-    return this.name;
-  }
-};
+    return this.name
+  },
+}
 
 var child = Object.create(parent, {
   name: {
     configurable: true,
     enumerable: true,
     value: 'Child',
-    writable: true
-  }
-});
+    writable: true,
+  },
+})
 
-export {
-  Person,
-  Rectangle,
-  Square,
-  parent,
-  child
-}
+export { Person, Rectangle, Square, parent, child }
