d332ced2c9a69e613af8b5635cfcab5252a41577
devJang <devjang2016@gmail.com>
2018-05-07 13:09:59 +0900

    Update: Resolve rewriteClass

--- src/04-30-class-second-part/rewriteClass.ts
+++ src/04-30-class-second-part/rewriteClass.ts
@@ -3,19 +3,15 @@
  * 
  * @param name 
  */
-function Person(name) {
-  // 생성자내에서만 사용가능
-  var age = 30;
-  this.name = name;
+class Person {
+  private age: number = 30;
+
+  constructor(public name: string) {}
   
-  this.greeting = function() {
+  greeting() {
     return 'My name is ' + this.name;
   };
 }
-
-Person.prototype.greeting = function() {
-  return this.name;
-};
 
 /**
  * Exercise 2 - Inheritance
@@ -24,44 +20,46 @@ Person.prototype.greeting = function() {
  * @param width 
  */
 // 기본 클래스
-function Rectangle(length, width) {
-  this.length = length;
-  this.width = width;
-}
+class Rectangle {
+  length: number;
+  width: number;
 
-Rectangle.prototype.getArea = function() {
-  return this.length * this.width;
+  constructor(length: number, width: number) {
+    this.length = length;
+    this.width = width;
+  }
+
+  getArea() {
+    return this.length * this.width;
+  }
 }
 
 // 서브 클래스
-function Square(size) {
-  this.length = size;
-  this.width = size;
+class Square extends Rectangle {
+  constructor(size: number) {
+    super(size, size);
+  }
 }
 
 /**
  * Exercise 3 - Object Inheritance
  */
-var parent = {
-  name: 'Parent',
-  sayName: function() {
+class Parent {
+  name: string = 'Parent';
+  
+  sayName() {
     return this.name;
   }
 };
 
-var child = Object.create(parent, {
-  name: {
-    configurable: true,
-    enumerable: true,
-    value: 'Child',
-    writable: true
-  }
-});
+class Child extends Parent {
+  name: string = 'Child';
+};
 
 export {
   Person,
   Rectangle,
   Square,
-  parent,
-  child
+  Parent,
+  Child
 }
