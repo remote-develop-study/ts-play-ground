fe4efdddc348b44d0eb163e1e3e84a9b6a9e97cf
정주영 <nwwn1004@naver.com>
2018-05-07 22:25:23 +0900

    rewriteClass 공부 후 과제제출

--- src/04-23-class-first-part/rewriteClass.ts
+++ src/04-23-class-first-part/rewriteClass.ts
@@ -2,52 +2,73 @@
  * 3개의 클래스를 100% TS 클래스로 다시 작성해주세요
  */
 
-// Exercise 1 - Class
-function Car() {
-    this.name = name;
-    this.acceleration = 0;
-
-    this.honk = function() {
-        return "빵빵!";
-    };
-
-    this.accelerate = function(speed) {
-        this.acceleration = this.acceleration + speed;
+class Car{
+    accelration: number = 0;
+    constructor(public name: string){
+        console.log('Car constructor name: ',this.name);
+    }
+    honk(): string{
+        return '빵빵!';
+    }
+    accelerate(speed: number): void{
+        this.accelration = this.accelration + speed;
     }
 }
-
 // Exercise 2 - Inheritance
-var baseObject = {
-    width: 0,
-    length: 0
-};
+// var baseObject = {
+//     width: 0,
+//     length: 0
+// };
 
-var Rectangle = Object.create(baseObject);
+// var Rectangle = Object.create(baseObject);
 
-Rectangle.calcSize = function() {
-    return this.width * this.length;
-};
-
+// Rectangle.calcSize = function() {
+//     return this.width * this.length;
+// };
+class baseObject{
+    width:number = 0;
+    length:number = 0;
+}
+class Rectangle extends baseObject{
+    calcSize(): number{
+        return this.width * this.length;
+    }
+}
 // Exercise 3 - Getter & Setter
-var Person = {
-    _firstName: ""
-};
+// var Person = {
+//     _firstName: ""
+// };
 
-Object.defineProperty(Person, "firstName", {
-    get: function () {
-        return this._firstName;
-    },
-    set: function (value) {
-        if (value.length >= 3) {
-            this._firstName = value;
-        }
-        else {
+// Object.defineProperty(Person, "firstName", {
+//     get: function () {
+//         return this._firstName;
+//     },
+//     set: function (value) {
+//         if (value.length >= 3) {
+//             this._firstName = value;
+//         }
+//         else {
+//             this._firstName = "";
+//         }
+//     },
+//     enumerable: true,
+//     configurable: true
+// });
+
+type stringOrUndefined = string | undefined;
+class Person{
+    private _firstName: stringOrUndefined;
+    get firstName(): stringOrUndefined{
+        return this._firstName
+    }
+    set firstName(param:stringOrUndefined){
+        if(param.length >= 3){
+            this._firstName = param;
+        }else{
             this._firstName = "";
         }
-    },
-    enumerable: true,
-    configurable: true
-});
+    }
+}
 
 export {
   Car,
