a556a4ad3190e0cd8e5a76f43488ea560629deba
rudwnok123 <rudwnok123@naver.com>
2018-06-10 22:52:27 +0900

    dd

---
+++ src/06-04-undefined/ReserveMovie.js
@@ -0,0 +1,47 @@
+// import '../06-04-undefined/data.js';
+// import { getUserId } from '../01-30-interfaces/interfaces.js';
+var id = '';
+var pw = null;
+var id_ = [];
+var pw_ = [];
+function loginAlert() {
+    document.getElementById('presentID').innerHTML = id;
+}
+// 로그인
+function log(id, pw) {
+    id = document.getElementById('id').value;
+    pw = parseInt(document.getElementById('pw').value);
+    if (id == 'admin' && pw == 1111) {
+        //관리자가 영화데이터를 생성할수 있는 함수 넣기
+        loginAlert();
+    }
+    else if ((id != 'admin' || pw != 1111)) {
+        console.log('계정 로그인' + id);
+        loginAlert();
+    }
+    else {
+        console.log('가입정보가 없습니다');
+    }
+}
+//회원가입부분
+// 1. 계정이 있는지 없는지 판별하기. 없다면 만들고, 있다면 있다고 알림.
+function resgist() {
+    var id_length = id_.length;
+    for (var i = 0; i < id_length; i++) {
+        console.log(id_length);
+    }
+    id = document.getElementById('id').value;
+    pw = parseInt(document.getElementById('pw').value);
+    id_.push(id);
+    console.log(id);
+}
+// 예약부분
+var Reserve = /** @class */ (function () {
+    function Reserve(_movieName, _date, _time) {
+        this.moveName = _movieName;
+        this.date = _date;
+        this.time = _time;
+    }
+    return Reserve;
+}());
+// let reserve = new Reserve("영화이름1", '06.20','12:00');
--- src/06-04-undefined/ReserveMovie.ts
+++ src/06-04-undefined/ReserveMovie.ts
@@ -1,2 +1,65 @@
-import '../06-04-undefined/data.json';
+// import '../06-04-undefined/data.js';
+// import { getUserId } from '../01-30-interfaces/interfaces.js';
+
+
+
+
+
+let id:string = '';
+let pw:number | null = null;
+let id_:any[] = [];
+let pw_:any[] = [];
+
+function loginAlert(){
+  (<HTMLInputElement>document.getElementById('presentID')).innerHTML = id;
+}
+// 로그인
+function log(id: string, pw: number | string):void {
+  id = (<HTMLInputElement>document.getElementById('id')).value;
+  pw = parseInt((<HTMLInputElement>document.getElementById('pw')).value);
+  if (id == 'admin' && pw == 1111) {
+    //관리자가 영화데이터를 생성할수 있는 함수 넣기
+    loginAlert();
+  
+  }
+  // 회원 로그인하기
+  
+  else if((id != 'admin' || pw != 1111)){
+    console.log('계정 로그인'+id);
+    loginAlert();
+  }
+  else{
+    console.log('가입정보가 없습니다');
+  }
+}
+
+//회원가입부분
+// 1. 계정이 있는지 없는지 판별하기. 없다면 만들고, 있다면 있다고 알림.
+function resgist():void{
+  let id_length = id_.length;
+  for(let i=0;i<id_length;i++){
+    console.log(id_length);
+  }
+  id = (<HTMLInputElement>document.getElementById('id')).value;
+  pw = parseInt((<HTMLInputElement>document.getElementById('pw')).value);
+  id_.push(id);
+  console.log(id);
+}
+
+// 예약부분
+class Reserve {
+  moveName : string;
+  date : string | number;
+  time : string | number;
+  constructor(_movieName:string,_date:string|number,_time:string | number) {
+    this.moveName = _movieName;
+    this.date = _date;
+    this.time = _time;
+  }
+
+}
+
+// let reserve = new Reserve("영화이름1", '06.20','12:00');
+
+
 
