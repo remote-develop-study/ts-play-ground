6e28b9bbf409f0d040b7971eca2ea2b25bb8c9d1
kyoung-ju <rudwnok123@naver.com>
2018-06-11 17:13:52 +0900

    구조 생성

--- src/06-04-undefined/ReserveMovie.js
+++ src/06-04-undefined/ReserveMovie.js
@@ -1,16 +1,26 @@
-// import '../06-04-undefined/data.js';
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// import * as movie from "./data.js";
 // import { getUserId } from '../01-30-interfaces/interfaces.js';
 var movie = {
     ListData: [
         {
             name: '영화제목1',
-            date: '날짜',
+            date: '18-02-01',
             time: '23:00'
         },
         {
-            name: '상품문의',
-            date: '1제목이 출력되겠지요 한줄일때는 이렇게',
-            time: '다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파'
+            name: '영화제목2',
+            date: '18-03-11',
+            time: '11:00'
         }
     ]
 };
@@ -20,17 +30,20 @@ var id = '';
 var pw = null;
 var id_ = [];
 var pw_ = [];
+// 로그인시 로그인된거 표기
 function loginAlert() {
-    document.getElementById('presentID').innerHTML = id;
+    document.getElementById('presentID').innerHTML = id + '님 환영합니다.';
 }
 // 로그인
 function log(id, pw) {
     id = document.getElementById('id').value;
     pw = parseInt(document.getElementById('pw').value);
+    // @1111이 아닌 0000으로 했을 시 오류뜸
     if (id == 'admin' && pw == 1111) {
         //관리자가 영화데이터를 생성할수 있는 함수 넣기
         loginAlert();
     }
+    // 회원 로그인하기
     else if ((id != 'admin' || pw != 1111)) {
         console.log('계정 로그인' + id);
         loginAlert();
@@ -52,12 +65,43 @@ function resgist() {
     console.log(id);
 }
 // 예약부분
+// 영화 정보 기본값0
 var Reserve = /** @class */ (function () {
     function Reserve(_movieName, _date, _time) {
-        this.moveName = _movieName;
+        this.movieName = _movieName;
         this.date = _date;
         this.time = _time;
     }
+    // 예약이 가능한 판별은 여기서 하고 
+    Reserve.prototype.selectMovie = function () {
+    };
+    Reserve.prototype.selectDate = function () {
+    };
+    Reserve.prototype.selectTime = function () {
+    };
     return Reserve;
 }());
-// let reserve = new Reserve("영화이름1", '06.20','12:00');
+// 가능하다면 하위 클래스로 이동
+var ReserveMovieName = /** @class */ (function (_super) {
+    __extends(ReserveMovieName, _super);
+    function ReserveMovieName() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ReserveMovieName;
+}(Reserve));
+// 날짜 선택 부분.
+var ReserveDate = /** @class */ (function (_super) {
+    __extends(ReserveDate, _super);
+    function ReserveDate() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ReserveDate;
+}(Reserve));
+// 시간 선택 부분.
+var ReserveTime = /** @class */ (function (_super) {
+    __extends(ReserveTime, _super);
+    function ReserveTime() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return ReserveTime;
+}(Reserve));
--- src/06-04-undefined/ReserveMovie.ts
+++ src/06-04-undefined/ReserveMovie.ts
@@ -1,56 +1,60 @@
-// import '../06-04-undefined/data.js';
+// import * as movie from "./data.js";
 // import { getUserId } from '../01-30-interfaces/interfaces.js';
 let movie = {
   ListData: [
     {
       name: '영화제목1',
-      date: '날짜',
+      date: '18-02-01',
       time: '23:00'
     },
     {
-      name: '상품문의',
-      date: '1제목이 출력되겠지요 한줄일때는 이렇게',
-      time: '다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파 다람쥐 헌 쳇바퀴에 돌고파'
+      name: '영화제목2',
+      date: '18-03-11',
+      time: '11:00'
     }
   ]
 }
+
 let a = movie.ListData[0].name;
 
 console.log(a);
 
-let id:string = '';
-let pw:number | null = null;
-let id_:any[] = [];
-let pw_:any[] = [];
+let id: string = '';
+let pw: number | null = null;
+let id_: any[] = [];
+let pw_: any[] = [];
 
-function loginAlert(){
-  (<HTMLInputElement>document.getElementById('presentID')).innerHTML = id;
+// 로그인시 로그인된거 표기
+function loginAlert() {
+  (<HTMLInputElement>document.getElementById('presentID')).innerHTML = id + '님 환영합니다.';
 }
+
 // 로그인
-function log(id: string, pw: number | string):void {
+function log(id: string, pw: number | string): void {
   id = (<HTMLInputElement>document.getElementById('id')).value;
   pw = parseInt((<HTMLInputElement>document.getElementById('pw')).value);
-  if (id == 'admin' && pw == 1111) {
+  // @1111이 아닌 0000으로 했을 시 오류뜸
+  if (id == 'admin' && pw == 1111){
     //관리자가 영화데이터를 생성할수 있는 함수 넣기
     loginAlert();
-  
+    
   }
   // 회원 로그인하기
-  
-  else if((id != 'admin' || pw != 1111)){
-    console.log('계정 로그인'+id);
+
+  else if ((id != 'admin' || pw != 1111)) {
+    console.log('계정 로그인' + id);
     loginAlert();
   }
-  else{
+  else {
     console.log('가입정보가 없습니다');
   }
 }
 
 //회원가입부분
 // 1. 계정이 있는지 없는지 판별하기. 없다면 만들고, 있다면 있다고 알림.
-function resgist():void{
+function resgist(): void {
   let id_length = id_.length;
-  for(let i=0;i<id_length;i++){
+  for (let i = 0; i < id_length; i++) {
     console.log(id_length);
   }
   id = (<HTMLInputElement>document.getElementById('id')).value;
@@ -60,19 +64,40 @@ function resgist():void{
 }
 
 // 예약부분
+// 영화 정보 기본값0
 class Reserve {
-  moveName : string;
-  date : string | number;
-  time : string | number;
-  constructor(_movieName:string,_date:string|number,_time:string | number) {
-    this.moveName = _movieName;
+  movieName: string;
+  date: string | number;
+  time: string | number;
+  constructor(_movieName: string, _date: string | number, _time: string | number) {
+    this.movieName = _movieName;
     this.date = _date;
     this.time = _time;
   }
+  // 예약이 가능한 판별은 여기서 하고 
+  selectMovie() {
+
+  }
+  selectDate() {
+
+  }
+  selectTime() {
+
+  }
+}
+
+// 가능하다면 하위 클래스로 이동
+class ReserveMovieName extends Reserve {
+}
+// 날짜 선택 부분.
+class ReserveDate extends Reserve {
+
+}
+// 시간 선택 부분.
+class ReserveTime extends Reserve {
 
 }
 
-// let reserve = new Reserve("영화이름1", '06.20','12:00');
 
 
 
